a0b80b060f18e199d96793fa982914746b7cc7bf
Anton Kuzmichev <Antonkuzmichev17@gmail.com>
2026-05-15 20:41:39 -0400

    feat: add LruCache struct and constructor

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+mod hash_map;
+mod linked_list;
+
+use hash_map::HashMap;
+use linked_list::DoublyLinkedList;
+use std::hash::Hash;
+
+pub struct LruCache<K, V> {
+    map: HashMap<K>,
+    list: DoublyLinkedList<K, V>,
+    capacity: usize,
+}
+
+impl<K: Hash + Eq + Clone, V> LruCache<K, V> {
+    pub fn new(capacity: usize) -> Self {
+        assert!(capacity > 0, "LruCache capacity must be greater than zero");
+
+        Self {
+            map: HashMap::new(),
+            list: DoublyLinkedList::new(),
+            capacity,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.list.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.list.is_empty()
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
 }
